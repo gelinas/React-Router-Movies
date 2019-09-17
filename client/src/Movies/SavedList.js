fe96fc826430456645276f1dd2568784ed6144ab
gelinas <jlgelinas@gmail.com>
2019-09-17 17:42:01 -0500

    MVP and all stretch

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom'
 
-const SavedList = props => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+
+const SavedList = (props) => {
+
+  return(
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map((movie, index) => (
+        <NavLink key={index} className="saved-movie" to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+      ))}
+      <Link to="/">
+        <div className="home-button">Home</div>
+      </Link>
+      
+    </div>
+  );
+} 
 
 export default SavedList;
